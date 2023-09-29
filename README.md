0c3d5f713a1bace895ec7cdbb41b36263b7bb287
karthickeyan-mediwave <karthickeyan@mindwaveventures.com>
2023-09-29 11:18:57 +0530

    script- add

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-## jquery-api
+## ajax
+
+### fetch dog image from dog.ceo urls by using ajax
+
+```sh
+$ To run npx serve app
+```
--- app/script.js
+++ app/script.js
@@ -1,57 +1,62 @@
-// $(document).ready(function(){
+$.ajax({
+  url: "https://dog.ceo/api/breeds/list/all",
+  type: "GET",
+  success: function (response) {
+    if (response.status == "success") {
+      let jsonResponse = response.message;
+      for (let key in jsonResponse) {
+        let breedName = key;
+        if (jsonResponse[key].length > 0) {
+          for (let i = 0; i < jsonResponse[key].length; i++) {
+            let dogName = jsonResponse[key][i];
+            $("#dog-select").append(
+              `<option value="${breedName + "/" + dogName}">${
+                dogName + " " + breedName
+              }</option>`
+            );
+            console.log("Api request success");
+          }
+        } else {
+          $("#dog-select").append(
+            `<option value="${breedName}">${breedName}</option>`
+          );
+        }
+      }
+    }
+  },
+  error: function () {
+    console.log("Api request error");
+  },
+  complete: function () {
+    console.log("API request completed");
+    $("#load").hide();
+    $("#container").show();
+  },
+});
+$("#fetchImage").click(getDogImage);
+function getDogImage() {
+  let dogName = $("#dog-select").val();
+  $("#load").show();
+  $("#container").hide();
+  $.ajax({
+    url: "https://dog.ceo/api/breed/" + dogName + "/images/random",
+    type: "GET",
+    success: function (response) {
+      $("#load").hide();
 
-// })
-$("#load").show();
-$("#container").hide();
-function getAllDogsFromApi() {
-  // https://dog.ceo/api/breeds/list/all
-  const url = "https://dog.ceo/api/breeds/list/all";
-  $.ajax(url, {
-    method: "GET",
-    start: function () {
-      console.log("start");
-      $("#load").show();
-      $("#container").hide();
-    },
-    success: function (resp) {
-      console.log("Api request success");
-      console.log(resp);
-      console.log(Object.keys(resp.message));
+      if (response.status == "success") {
+        $("#dog-image").attr("src", response.message);
+        $("#dog-image").css({ height: "200px", width: "200px" });
+        console.log("Api request success");
+      }
     },
     error: function () {
       console.log("Api request error");
     },
     complete: function () {
-      console.log("API request completed");
+      console.log("API request completed img");
       $("#load").hide();
       $("#container").show();
     },
   });
 }
-function getRandomImageOfDog(dogBreed) {
-  // https://dog.ceo/api/breed/affenpinscher/images/random
-  const url = `https://dog.ceo/api/breed/affenpinscher/images/random`;
-  //   const url = "https://dog.ceo/api/breeds/image/random";
-  $.ajax(url, {
-    method: "GET",
-    success: function (resp) {
-      console.log("Api request success");
-      console.log(resp);
-    },
-    error: function () {
-      console.log("Api request error");
-    },
-  });
-}
-
-getAllDogsFromApi();
-getRandomImageOfDog();
-console.log("hiii");
-
-// let kk = $("#").val();
-
-console.log(kk);
-$("#getimg").click(function () {
-  let kk = $("#selection").val();
-  document.location.href = kk;
-});
